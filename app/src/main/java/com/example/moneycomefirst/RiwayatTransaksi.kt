223b101c7a3ffc916df2796d658d0559744c416d
Nafisah Faza <nafisahfazaq@gmail.com>
2021-05-24 21:52:45 +0700

    change transaksi to riwayat transaksi

--- app/src/main/java/com/example/moneycomefirst/RiwayatTransaksi.kt
+++ app/src/main/java/com/example/moneycomefirst/RiwayatTransaksi.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Transaksi.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Transaksi : Fragment() {
+class RiwayatTransaksi : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Transaksi : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaksi, container, false)
+        return inflater.inflate(R.layout.fragment_riwayat_transaksi, container, false)
     }
 
     companion object {
