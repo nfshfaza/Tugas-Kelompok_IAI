c00c5038d080973462c98bee0b78f8bc77e5c105
Nafisah Faza <nafisahfazaq@gmail.com>
2021-06-05 14:47:16 +0700

    add drawer navbar

--- app/src/main/java/com/example/moneycomefirst/MainActivity.kt
+++ app/src/main/java/com/example/moneycomefirst/MainActivity.kt
@@ -3,43 +3,53 @@ package com.example.moneycomefirst
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var profileCircleImageView: CircleImageView
+    private var profileImageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gemtvasia.com%2F7-things-you-didnt-know-about-mackenyu-arata&psig=AOvVaw2aNhJXJwDLwzb8KZR2q7qp&ust=1622963131554000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjEgoH3__ACFQAAAAAdAAAAABAP"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navController = Navigation.findNavController(this,R.id.fragment)
-        val navView: DrawerLayout = findViewById(R.id.nav_view)
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.favouritesFragment, R.id.homeFragment, R.id.profileFragment
-        ).build()
-        setupActionBarWithNavController(navController,appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        //cara menentukan bottom navbar ada di fragment mana aja
-//        navController.addOnDestinationChangedListener{ _, nd: NavDestination, _ ->
-//            when(nd.id){
-//                R.id.favouriteDetailFragment,
-//                R.id.foodDetailsFragment,
-//                R.id.menuFragment -> navView.visibility = View.GONE
-//                else -> navView.visibility = View.VISIBLE
-//            }
-//        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        profileCircleImageView = navView.getHeaderView(0).findViewById(R.id.iv_navbarProfPict)
+        Glide.with(this)
+            .load(profileImageUrl)
+            .into(profileCircleImageView)
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.home, R.id.riwayatTransaksi, R.id.tambahSaldo), drawerLayout)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        //untuk mengatur judul AppBar agar sesuai dengan Fragment yang ditampilkan.
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        //supaya NavigationDrawer menampilkan Fragment yang sesuai ketika menu dipilih.
+        navView.setupWithNavController(navController)
     }
 
     //cara mengaktifkan tombol back
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, null)
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
